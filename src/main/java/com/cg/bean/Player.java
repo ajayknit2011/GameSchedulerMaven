307aa5322e1baa538dfc9dbedf6273a68c6b3720
ajayknit2011 <35163550+ajayknit2011@users.noreply.github.com>
2018-09-27 16:08:17 +0530

    Add files via upload

--- src/main/java/com/cg/bean/Player.java
+++ src/main/java/com/cg/bean/Player.java
@@ -4,6 +4,58 @@ import java.util.Set;
 
 public class Player {
 
+	private String playerName;
+	private Set<Game> games;
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((games == null) ? 0 : games.hashCode());
+		result = prime * result + ((playerName == null) ? 0 : playerName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (games == null) {
+			if (other.games != null)
+				return false;
+		} else if (!games.equals(other.games))
+			return false;
+		if (playerName == null) {
+			if (other.playerName != null)
+				return false;
+		} else if (!playerName.equals(other.playerName))
+			return false;
+		return true;
+	}
+	
+
+	public Player() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Player(String playerName, Set<Game> games) {
+		super();
+		this.playerName = playerName;
+		this.games = games;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [playerName=" + playerName + ", games=" + games + "]";
+	}
+
 	public String getPlayerName() {
 		return playerName;
 	}
@@ -20,6 +72,4 @@ public class Player {
 		this.games = games;
 	}
 
-	private String playerName;
-	private Set<Game> games;
 }
