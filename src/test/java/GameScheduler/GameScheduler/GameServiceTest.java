9c881dd0a5f4afa0d3be4a6ad158ea6ce59fe4cb
ajayknit2011 <35163550+ajayknit2011@users.noreply.github.com>
2018-09-27 16:11:35 +0530

    Add files via upload

--- src/test/java/GameScheduler/GameScheduler/GameServiceTest.java
+++ src/test/java/GameScheduler/GameScheduler/GameServiceTest.java
@@ -14,6 +14,8 @@ import org.mockito.MockitoAnnotations;
 
 import com.cg.bean.Day;
 import com.cg.bean.Game;
+import com.cg.bean.Player;
+import com.cg.exception.DuplicateEntriesException;
 import com.cg.repo.DayRepo;
 import com.cg.repo.GameRepo;
 import com.cg.repo.PlayerRepo;
@@ -31,7 +33,9 @@ public class GameServiceTest {
 	private PlayerService servicep;
 	@Mock
 	private GameRepo repo;
+	@Mock
 	private DayRepo  repod;
+	@Mock
 	private PlayerRepo repop;
 
 	@Before
@@ -41,109 +45,119 @@ public class GameServiceTest {
 		service = new GameServiceImpl(repo);
 		serviced = new DayServiceImpl(repod);
 		servicep = new PlayerServiceImpl(repop);
-		
 	}
-	
-		@Test
+	@Test(expected = NullPointerException.class)
 	public void test_createGame_nullValue() {
 		Game game = null;
-            
-		when(repo.save(game)).thenReturn(null);
-		
-		assertEquals(game,null);
-		
+		when(repo.save(game)).thenReturn(game);
+		assertEquals(game,service.add(null));
 	}
-	
 	@Test
 	public void test_createGame_success() {
 		Game game = new Game("Kabaddi", (byte)7);
-            
 		when(repo.save(game)).thenReturn(game);
-		
 		assertEquals(game, service.add(new Game("Kabaddi", (byte)7)));
-	
 	}
-	
-	@Test
+
+	@Test(expected=DuplicateEntriesException.class)
 	public void test_createGame_sameName() {
-		Game g1=new Game("Kabaddi",(byte)7);
-		Game g2=new Game("Kabaddi",(byte)7);
-		assertNotSame(g1, g2);
+		Game game = new Game("Kabaddi", (byte)7);
+		Game game1 = new Game("Kabaddi", (byte)7);
+		when(repo.findByName(game.getGameName())).thenReturn(game);
+		service.add(game);
+	
 	}
-	
-	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_createGame_throwsExceptionForIlllegalArguments() {
-
 		//service.add(new Game("Kabaddi", (byte) 11));
 		service.add(new Game(null, (byte) 11));
 		service.add(new Game("Kabaddi", (byte) 0));
-
 	}
+	
+	
+	/*..........................................DAYService.......................................*/
+	
 
-	
-	
-	
-	
-	
-	
-	
-	@Test
+	@Test(expected = NullPointerException.class)
 	public void test_createDay_nullValue() {
 		Day day = null;
-            
-		when(repod.save(day)).thenReturn(null);
-		
-		assertEquals(day,null);
-		
+		when(repod.save(day)).thenReturn(day);
+		assertEquals(day,serviced.add(day));
 	}
-	
 	@Test
 	public void test_createDay_success() {
 		Set<Game> set=new HashSet<Game>();
 		set.add(new Game("Kabaddi", (byte)7));
-	//	set.add(new Game("Cricket", (byte)7));
-	//	set.add(new Game("KhoKho", (byte)7));
-		
-		Day day = new Day("one",set );
-            
+		set.add(new Game("Cricket", (byte)11));
+		set.add(new Game("KhoKho", (byte)7));
+		Day day = new Day();
+		day.setDayName("day1");
+		day.setGames(set);
 		when(repod.save(day)).thenReturn(day);
-		
-		assertEquals(day, serviced.add(new Day("one",set)));
-	
+		serviced.add(day);
 	}
-	
-	@Test
+	@Test(expected=DuplicateEntriesException.class)
 	public void test_createDay_sameName() {
 		Set<Game> set=new HashSet<Game>();
 		set.add(new Game("Kabaddi", (byte)7));
-		Set<Game> set2=new HashSet<Game>();
-		set2.add(new Game("Kabaddi", (byte)7));
-	//	set.add(new Game("Cricket", (byte)7));
-	//	set.add(new Game("KhoKho", (byte)7));
-	//	Game g1=new Game("Kabaddi",(byte)7);
-	//	Game g2=new Game("Kabaddi",(byte)7);
-		Day d1=new Day("one",set );
-		Day d2=new Day("one",set );
-		assertNotSame(d1, d2);
+		set.add(new Game("Cricket", (byte)7));
+		set.add(new Game("KhoKho", (byte)7));
+		Day day = new Day();
+		day.setDayName("day1");
+		day.setGames(set);
+		when(repod.findByName(day.getDayName())).thenReturn(day);
+		serviced.add(day);
 	}
-	
-	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_createDay_throwsExceptionForIlllegalArguments() {
-
 		Set<Game> set=new HashSet<Game>();
 		set.add(new Game("Kabaddi", (byte)7));
-	//	set.add(new Game("Cricket", (byte)7));
-	//	set.add(new Game("KhoKho", (byte)7));
-		//service.add(new Game("Kabaddi", (byte) 11));
+		set.add(new Game("Cricket", (byte)7));
+		set.add(new Game("KhoKho", (byte)7));
 		serviced.add(new Day(null,set));
 		serviced.add(new Day("one",null));
-
 	}
-
 	
 	
+/*.................................Player Service......................*/
 	
-	
+	@Test(expected = NullPointerException.class)
+	public void test_createPlayer_nullValue() {
+		Player player = null;
+		when(repop.save(player)).thenReturn(player);
+		assertEquals(player,servicep.add(player));
+	}
+	@Test
+	public void test_createPlayer_success() {
+		Set<Game> set=new HashSet<Game>();
+		set.add(new Game("Kabaddi", (byte)7));
+		set.add(new Game("Cricket", (byte)11));
+		set.add(new Game("KhoKho", (byte)7));
+		Player player = new Player();
+		player.setPlayerName("ajay");
+		player.setGames(set);
+		when(repop.save(player)).thenReturn(player);
+		servicep.add(player);
+	}
+	@Test(expected=DuplicateEntriesException.class)
+	public void test_createPlayer_sameName() {
+		Set<Game> set=new HashSet<Game>();
+		set.add(new Game("Kabaddi", (byte)7));
+		set.add(new Game("Cricket", (byte)7));
+		set.add(new Game("KhoKho", (byte)7));
+		Player player = new Player();
+		player.setPlayerName("ajay");
+		player.setGames(set);
+		when(repop.findByName(player.getPlayerName())).thenReturn(player);
+		servicep.add(player);
+	}
+	@Test(expected = NullPointerException.class)
+	public void test_createPlayer_throwsNullPointerException() {
+		Set<Game> set=new HashSet<Game>();
+		set.add(new Game("Kabaddi", (byte)7));
+		set.add(new Game("Cricket", (byte)7));
+		set.add(new Game("KhoKho", (byte)7));
+		servicep.add(new Player("ajay",null));
+		servicep.add(new Player(null,set));
+	}
 } 
